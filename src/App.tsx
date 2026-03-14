b77c5558d36d9611dc3f3dac7ad5e3ac3ad71401
Vinicyos Ferreira <vinifr987@gmail.com>
2026-03-14 18:57:43 -0300

    chore: update dependencies and routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { Header } from "./components/Header"
-import { Hero } from "./components/Hero"
-import { CardsSection } from "./components/CardsSection"
-import { VideoSection } from "./components/VideoSection"
-import { Footer } from "./components/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Layout } from "./components/Layout"
+import { HomePage } from "./pages/HomePage"
+import { AboutPage } from "./pages/AboutPage"
+import { BenefitsPage } from "./pages/BenefitsPage"
+import { TestimonialsPage } from "./pages/TestimonialsPage"
 
 const App = () => {
   return (
-    <main className="min-h-screen flex flex-col bg-background text-foreground">
-      <Header />
-      <Hero />
-      <CardsSection />
-      <VideoSection />
-      <Footer />
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="benefits" element={<BenefitsPage />} />
+          <Route path="testimonials" element={<TestimonialsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
